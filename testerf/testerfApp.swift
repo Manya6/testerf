31a6e5ccd277a7b057fb05d4fe7a849884b0e21e
Manya Malhotra <malhotra.manyaaaa@gmail.com>
2024-07-25 10:27:04 +0100

    Initial Commit

---
+++ testerf/testerfApp.swift
@@ -0,0 +1,17 @@
+//
+//  testerfApp.swift
+//  testerf
+//
+//  Created by Manya Malhotra on 25/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct testerfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
